c86e6231e43ccfb7789e3a53c8873788df1a0ec4
Brandon <brandon.teague5@gmail.com>
2020-09-30 19:43:27 -0400

    created basic login and register functionality

--- pages/register/index.js
+++ pages/register/index.js
@@ -1,9 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Register = () => {
+
+  const [credentials, setCredentials] = useState({
+    email: '',
+    password: ''
+  })
+
+  const handleChange = (e) => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post(``, credentials)
+      .then(res => {
+
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
   return (
     <div className="register">
-      <h1>Register</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="first_name"
+          onChange={handleChange}
+          placeholder="First Name"
+        />
+        <input
+          type="text"
+          name="last_name"
+          onChange={handleChange}
+          placeholder="Last Name"
+        />
+        <input
+          type="text"
+          name="email"
+          onChange={handleChange}
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          name="password"
+          onChange={handleChange}
+          placeholder="Password"
+        />
+        <button>Register</button>
+      </form>
     </div>
   );
 }
