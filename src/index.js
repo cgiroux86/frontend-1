e7559b8aac8efaabf1d13787f4620d1287cd185f
Brandon <brandon.teague5@gmail.com>
2020-10-04 21:07:29 -0400

    connected router

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { RecoilRoot } from "recoil";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <RecoilRoot>
     <App />
-  </React.StrictMode>,
+  </RecoilRoot>,
   document.getElementById('root')
 );
 
